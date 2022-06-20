7ed0bb6a72623acf300244c881991f9025cb6e9b
Hunor Kovacs <kovacshuni@gmail.com>
2022-06-21 01:53:12 +0200

    About Eq. Until chapter 1.6.1

--- itselen-simple/src/main/scala/com/hunorkovacs/itsaren/simple/ItsElenApp.scala
+++ itselen-simple/src/main/scala/com/hunorkovacs/itsaren/simple/ItsElenApp.scala
@@ -2,20 +2,32 @@ package com.hunorkovacs.itselen.simple
 
 import cats.effect.IO
 import cats.effect.unsafe.implicits.global
-import com.typesafe.config.ConfigFactory
-import io.circe.config.parser
+import cats.Eq
+import cats.instances.long._
+import cats.syntax.eq._
+import cats.syntax.option._
+import java.util.Date
 import org.typelevel.log4cats._
 import org.typelevel.log4cats.slf4j._
-import io.circe.syntax._
 
 object ItsElenApp:
 
   private val logger: SelfAwareStructuredLogger[IO] = LoggerFactory[IO].getLogger
 
   def main(args: Array[String]): Unit =
-    val ops = for
-      config <- IO(ConfigFactory.load()).flatMap(typesafeConfig => parser.decodePathF[IO, ItsElenConfig](typesafeConfig, "itselen-sample"))
-      _      <- logger.info(s"configuration: ${config.asJson.spaces2}")
-      _      <- IO(println("hi"))
-    yield ()
-    ops.unsafeRunSync()
+    println("hi")
+
+  implicit val dateEq: Eq[Date] =
+    Eq.instance[Date] { (date1, date2) => date1.getTime == date2.getTime }
+
+  final case class Cat(name: String, age: Int, color: String)
+
+  val cat1 = Cat("Garfield", 38, "orange and black")
+  val cat2 = Cat("Heathcliff", 33, "orange and black")
+
+  implicit val catEq: Eq[Cat] =
+    Eq.instance[Cat] { (cat1, cat2) =>
+      cat1.age == cat2.age &&
+      cat1.color == cat2.color &&
+      cat1.name == cat2.name
+    }
