f5af3bd54e3c095868402630e179570417d5f30f
Hunor Kovacs <kovacshuni@gmail.com>
2022-06-23 11:48:20 +0200

    About covariance and contravariance. Chapter 1.6.1

--- itselen-simple/src/main/scala/com/hunorkovacs/itsaren/simple/ItsElenApp.scala
+++ itselen-simple/src/main/scala/com/hunorkovacs/itsaren/simple/ItsElenApp.scala
@@ -15,7 +15,34 @@ object ItsElenApp:
   private val logger: SelfAwareStructuredLogger[IO] = LoggerFactory[IO].getLogger
 
   def main(args: Array[String]): Unit =
-    println("hi")
+    val circles: List[Circle]     = List(Circle(1.0d), Circle(2.0d))
+    val shapes: List[Shape]       = circles
+    val circleOpt: Option[Circle] = Option(Circle(3.0d))
+    val shapeOpt: Option[Shape]   = circleOpt
+    val circleMon: MyMon[Circle]  = MyMon(Circle(4.0d))
+    val shapeMon: MyMon[Shape]    = MyMon(Circle(4.0d))
+    val mycvShape: MyCv[Shape]    = new MyCv[Shape] {
+      override def write(s: Shape): String = s.name
+    }
+    val mycvCircle: MyCv[Circle]  = mycvShape
+
+    println(mycvCircle.write(Circle(4.0d)))
+    println(format(Circle(4.0d), mycvCircle))
+    println(format(Circle(4.0d), mycvShape))
+    // println(format(Circle(4.0D).asInstanceOf[Shape], mycvCircle)) // can't do this
+    // MyCv[Shape] is a subtype of MyCv[Circle] because Circle is a subtype of Shape
+
+    val myIvCircle = new MyIv[Circle] {
+      override def ivWrite(c: Circle): String = c.name
+    }
+    val myIvShape  = new MyIv[Shape] {
+      override def ivWrite(s: Shape): String = s.name
+    }
+    println(ivFormat(Circle(4.0d), myIvCircle))
+    println(ivFormat(Circle(4.0d).asInstanceOf[Shape], myIvShape))
+    println(ivFormat(Circle(4.0d), myIvShape))
+    // println(ivFormat(Circle(4.0d).asInstanceOf[Shape], myIvCircle)) //nothing converts to nothing
+
 
   implicit val dateEq: Eq[Date] =
     Eq.instance[Date] { (date1, date2) => date1.getTime == date2.getTime }
@@ -31,3 +58,29 @@ object ItsElenApp:
       cat1.color == cat2.color &&
       cat1.name == cat2.name
     }
+
+  sealed trait Shape {
+    def name: String
+  }
+
+  case class Circle(radius: Double) extends Shape {
+    override def name: String = s"Circle with radius=$radius"
+  }
+
+  case class MyMon[+A](a: A) // covariance
+
+  trait MyCv[-A] { // contravariance
+    def write(a: A): String
+  }
+
+  class ShapeWriter extends MyCv[Shape] {
+    override def write(shape: Shape): String = s"Shape ${shape.name}"
+  }
+
+  def format[A](a: A, aCv: MyCv[A]): String = aCv.write(a)
+
+  trait MyIv[A] {
+    def ivWrite(a: A): String
+  }
+
+  def ivFormat[A](a: A, aIv: MyIv[A]): String = aIv.ivWrite(a)
